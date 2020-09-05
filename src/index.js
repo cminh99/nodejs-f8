d99c2d698ec8f5d4c58e6014c8ab8c7f1f7f7ae8
nhcminh303 <nhcminh303@gmail.com>
2020-09-05 23:10:50 +0700

    install & configure template engine [handlebars]

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 const port = 3000;
 
 const app = express();
 
+// setup template engine
+app.engine(
+  'hbs',
+  handlebars({
+    extname: '.hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+// global middlewares
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.render('home'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

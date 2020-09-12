24d9ea8e84937e18b7cfeb80dbe08c1c459a58d2
nhcminh303 <nhcminh303@gmail.com>
2020-09-12 16:13:03 +0700

    Delete course [* Kien thuc hay ho]

--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -9,6 +9,8 @@ router.post('/store', controller.store);
 router.get('/edit/:id', controller.edit);
 router.put('/update/:id', controller.update);
 
+router.delete('/delete/:id', controller.delete);
+
 router.get('/:slug', controller.view);
 
 module.exports = router;
